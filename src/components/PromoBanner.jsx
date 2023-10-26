66abb7a4b8168a8d4affae01fccbe6f3f0ed3615
Max <48150531+mashtakovmm@users.noreply.github.com>
2023-10-26 21:59:23 +0300

    add final screen

--- src/components/PromoBanner.jsx
+++ src/components/PromoBanner.jsx
@@ -4,6 +4,7 @@ import "./PromoBanner.css"
 import Keypad from './Keypad'
 import Button from '../UI/Button'
 import { useEffect, useReducer, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 
 function PromoBanner() {
@@ -12,6 +13,7 @@ function PromoBanner() {
     const [formatedPhoneNumber, setFormatedPhoneNumber] = useState('')
     const [checked, setChecked] = useState(false)
     const [buttonDisabled, setButtonDisabled] = useState(true)
+    const [accepted, setAccepted] = useState(false)
 
     function phoneNumberReducer(number, action) {
         switch (action.type) {
@@ -55,21 +57,45 @@ function PromoBanner() {
         setChecked(!checked)
     }
 
+    function handleSendInfo() {
+        if (!buttonDisabled) {
+            setAccepted(true)
+        }
+    }
+
 
     return (
         <>
-            <div className='promo-banner'>
-                <h2 className='h2-promo'>Введите ваш номер мобильного телефона</h2>
-                <input className='phone-input' type="tel" name='phone' placeholder='+7(___)___-__-__' value={formatedPhoneNumber} readOnly />
-                <p className='p-promo'>и с Вами свяжется наш менеждер для дальнейшей консультации</p>
-                <Keypad dispatcher={dispatchPhoneNumber} />
-                <div className='checkbox-container'>
-                    <div className="checkbox" onClick={handleCheckbox}>{checked && <img style={{ paddingTop: "6px" }} src={checkmark} />}</div>
-                    <label className='label-promo' htmlFor="">Согласие на обработку персональных данных</label>
+
+            {!accepted ? (
+                <div className='promo-banner'>
+                    <h2 className='h2-promo'>Введите ваш номер мобильного телефона</h2>
+                    <input className='phone-input' type="tel" name='phone' placeholder='+7(___)___-__-__' value={formatedPhoneNumber} readOnly />
+                    <p className='p-promo'>и с Вами свяжется наш менеджер для дальнейшей консультации</p>
+                    <Keypad dispatcher={dispatchPhoneNumber} />
+                    <div className='checkbox-container'>
+                        <div className="checkbox" onClick={handleCheckbox}>{checked && <img style={{ paddingTop: "6px" }} src={checkmark} />}</div>
+                        <label className='label-promo' htmlFor="">Согласие на обработку персональных данных</label>
+                    </div>
+                    <Button value="Подтвердить номер" className="accept-button" isDisabled={buttonDisabled} callback={handleSendInfo} />
                 </div>
-                <Button value="Подтвердить номер" className="accept-button" isDisabled={buttonDisabled} />
-            </div>
-            {/* TODO: fix qr code placement later */}
+            ) : (
+                <div className='promo-banner' style={{padding:"48px 46px;"}}>
+                    <div className='accepted-div'>
+                        <h1 className='accepted-h1'>ЗАЯВКА ПРИНЯТА</h1>
+                        <div>
+                            <p className='p-accepted'>Держите телефон под рукой.</p>
+                            <p className='p-accepted'>Скоро с Вами свяжется наш менеджер.</p>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+
+
+
+            {/* absolute components */}
+            <Link to={"/"}><Button value={"✕"} className="exit-button" /></Link>
             <div className='qr-container'>
                 <span className='qr-span'>Сканируйте QR-код ДЛЯ ПОЛУЧЕНИЯ ДОПОЛНИТЕЛЬНОЙ ИНФОРМАЦИИ</span>
                 <img className='qr-img' src={qr} alt="qr code" />
